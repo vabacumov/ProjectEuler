2974afc203d4bc0330c1314663344baff77781bf
Vlad Abacumov <vabacumov@gmail.com>
2014-12-04 12:24:31 -0800

    Adds refactored solution -- now able to find answer within a minute or so

--- vlad_problem_9.rb
+++ vlad_problem_9.rb
@@ -6,26 +6,7 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-def find_unique_pythagorean_triplet
-  array = (1..1000).to_a
-  a_index = 0
-  until a_index == 998
-    (a_index..998).to_a.each do |b_index|    
-      a = array[a_index]
-      b = array[b_index]
-      (b_index + 1..999).each do |c_index|
-        c = array[c_index]
-        p "a: #{a}, b: #{b}, c: #{c}"
-        return product(a,b,c) if is_a_pythagorean_triplet?(a,b,c) && is_a_sum_of_1000?(a,b,c)
-      end
-    end
-    a_index += 1
-  end
-end
-
-# numbers_range = (1..1000).to_a
-# square_roots_range = (1..1000).to_a.map! { |num| num ** 2 }
-
+######## HELPERS #############
 def is_a_pythagorean_triplet?(a,b,c)
   return true if a**2 + b**2 == c**2
 end
@@ -38,4 +19,52 @@ def product(*integers)
   return integers.inject(:*)
 end
 
-p find_unique_pythagorean_triplet
+###################### ORIGINAL SLOW SOLUTION ###################
+def find_unique_pythagorean_triplet
+  array = (1..1000).to_a
+  a_index = 0
+
+  until a_index == 998
+    (a_index..998).to_a.each do |b_index|    
+      a = array[a_index]
+      b = array[b_index]
+      (b_index + 1..999).each do |c_index|
+        c = array[c_index]
+
+        p "a: #{a}, b: #{b}, c: #{c}"
+        
+        return product(a,b,c) if is_a_pythagorean_triplet?(a,b,c) && is_a_sum_of_1000?(a,b,c)
+      end
+    end
+
+    a_index += 1
+  end
+end
+
+################# REFACTORED SOLUTION ################
+def find_unique_pythagorean_triplet_2
+  array = (1..1000).to_a
+  squared_values = (1..1000).to_a.map! { |value| value**2 }
+
+  c_index = 999
+
+  until c_index == 2
+    (1..c_index-1).to_a.reverse.each do |b_index|    
+      c = array[c_index]
+      b = array[b_index]
+
+      p "b-index: #{b_index}, c-index: #{c_index}"
+      p "b: #{array[b_index]}, c: #{array[c_index]}"
+
+      a_squared = c**2 - b**2
+
+      if squared_values.include?(a_squared) && is_a_sum_of_1000?(Math.sqrt(a_squared).to_i, array[b_index], array[c_index])
+        return product(Math.sqrt(a_squared).to_i, array[b_index], array[c_index])
+      end
+    end
+
+    c_index -= 1
+  end
+end
+
+p find_unique_pythagorean_triplet_2
